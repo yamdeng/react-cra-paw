208cb192d4bb8104e4ff209ed1e4c011c14eaeb7
user1 <jinwon.bae.job@gmail.com>
2024-07-22 15:42:53 +0900

    update #1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react-cra-paw
+
+npm install sql.js idb-keyval uuid crypto crypto-browserify fs localforage path path-browserify react-app-rewired
+
+# tailwindcss
+npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
--- src/App.js
+++ src/App.js
@@ -2,26 +2,138 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import OffLineApp from "./OffLineApp";
 import OnlineApp from "./OnlineApp";
+import localforage from 'localforage';
+
+//localForage.defineDriver(memoryDriver)
+//localforage.setDriver([localforage.LOCALSTORAGE, localforage.INDEXEDDB, localforage.WEBSQL])
+
+localforage.config({
+  driver: [
+    localforage.INDEXEDDB,
+    localforage.WEBSQL,
+    localforage.LOCALSTORAGE
+  ],
+  name: 'offline-storage'
+});
 
 const isFirstOnline = navigator.onLine;
 
 function App() {
-  const [isNetworkOnline, setIsNetworkOnline] = useState(isFirstOnline);
-  window.addEventListener("online", (event) => {});
 
-  let AppComponent = <OnlineApp />;
+  // const [base64String, setBase64String] = useState('');
+
+  // const fetchImageAsBase64 = async (imageUrl) => {
+  //   try {
+  //     // Fetch the image
+  //     const response = await fetch(imageUrl);
+  //     if (!response.ok) {
+  //       throw new Error(`Failed to fetch image: ${response.statusText}`);
+  //     }
+
+  //     // Convert the image to a blob
+  //     const blob = await response.blob();
+
+  //     // Convert the blob to a Base64 string
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => {
+  //       //console.log(reader.result.toString());
+  //       //setBase64String(reader.result);
+  //       //console.log(`base64String: ${base64String}`)
+  //       Array.from({ length: 10 }).map((_, index) => {
+  //         const saveData = async () => {
+  //           try {
+  //             await localforage.setItem(`myData + ${index}`, reader.result)
+  //             //setData(inputValue);
+  //           } catch (error) {
+  //             console.error('데이터를 저장하는 중 오류 발생:', error);
+  //           }
+  //         };
+  //         saveData()
+  //       })
+  //     };
+  //     reader.readAsDataURL(blob);
+  //   } catch (error) {
+  //     console.error('Error fetching or converting image:', error);
+  //   }
+  // };
+
+  const fetchImageAsBase642 = async (imageUrl) => {
+    try {
+      // Fetch the image
+      const response = await fetch(imageUrl);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch image: ${response.statusText}`);
+      }
+
+      // Convert the image to a blob
+      const blob = await response.blob();
+
+      // Convert the blob to a Base64 string
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const saveData = async () => {
+          try {
+            await localforage.setItem(`tar.bz2`, reader.result)
+            //setData(inputValue);
+          } catch (error) {
+            console.error('데이터를 저장하는 중 오류 발생:', error);
+          }
+        };
+        saveData()
+      };
+      reader.readAsDataURL(blob);
+    } catch (error) {
+      console.error('Error fetching or converting image:', error);
+    }
+  };
+
+  //  localforage.config({
+  //     driver      : localforage.WEBSQL, // Force WebSQL; same as using setDriver()
+  //     name        : 'myApp',
+  //     version     : 1.0,
+  //     size        : 4980736, // Size of database, in bytes. WebSQL-only for now.
+  //     storeName   : 'keyvaluepairs', // Should be alphanumeric, with underscores.
+  //     description : 'some description'
+  //   });
+
+  const [isNetworkOnline, setIsNetworkOnline] = useState(isFirstOnline);
+  window.addEventListener("online", (event) => { });
+
+  let AppComponent = <OffLineApp />;
   if (!isNetworkOnline) {
     AppComponent = <OffLineApp />;
   }
 
-  useEffect(() => {
-    window.addEventListener("online", (event) => {
-      setIsNetworkOnline(true);
-    });
-    window.addEventListener("offline", (event) => {
-      setIsNetworkOnline(false);
-    });
-  }, []);
+  // useEffect(() => {
+  //   window.addEventListener("online", (event) => {
+  //     setIsNetworkOnline(true);
+  //   });
+  //   window.addEventListener("offline", (event) => {
+  //     setIsNetworkOnline(false);
+  //   });
+
+  //   //const imageUrl = 'http://upload.eyine.com/uploads/2024_06_02_02_40_43.mp3'; // Replace with your image URL
+  //   //fetchImageAsBase64(imageUrl);
+
+  //   const imageUrl2 = 'https://dev3.eyine.com/test.app.tar.bz2'; // Replace with your image URL
+  //   fetchImageAsBase642(imageUrl2);
+
+  //   const imageUrl3 = 'https://dev3.eyine.com/driver.zip'; // Replace with your image URL
+  //   fetchImageAsBase643(imageUrl3);
+
+  //   // const fetchData = async () => {
+  //   //   try {
+  //   //     const storedData = await localforage.getItem('myData');
+  //   //     if (storedData) {
+  //   //       console.log(`storeData: ${storedData}`)
+  //   //     }
+  //   //   } catch (error) {
+  //   //     console.error('데이터를 불러오는 중 오류 발생:', error);
+  //   //   }
+  //   // };
+
+  //   // fetchData();
+  // }, []);
 
   return (
     <div className="App">
